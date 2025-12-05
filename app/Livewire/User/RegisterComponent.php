92cda6744b8a569f8200a136019b94ba998b237a
Vladislav <mel@mel.rr>
2025-12-05 13:14:36 +0300

    Регистрация пользователей

---
+++ app/Livewire/User/RegisterComponent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Livewire\User;
+
+use App\Models\User;
+use Livewire\Component;
+
+class RegisterComponent extends Component
+{
+    public string $name;
+    public string $email;
+    public string $password;
+
+    public function save()
+    {
+        $validated = $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|min:8',
+        ]);
+        $user = User::query()->create($validated);
+        session()->flash('success', 'Thanks for registration');
+        $this->redirectRoute('login', navigate: true);
+    }
+
+    public function render()
+    {
+        return view('livewire.user.register-component');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,16 @@
 use App\Livewire\HomeComponent;
 use App\Livewire\Product\CategoryComponent;
 use App\Livewire\Product\ProductComponent;
+use App\Livewire\User\LoginComponent;
+use App\Livewire\User\RegisterComponent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeComponent::class)->name('home');
 Route::get('/product/{slug}', ProductComponent::class)->name('product');
 Route::get('/category/{slug}', CategoryComponent::class)->name('category');
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', RegisterComponent::class)->name('register');
+    Route::get('/login', LoginComponent::class)->name('login');
+
+});
