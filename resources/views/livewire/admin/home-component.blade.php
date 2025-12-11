e5cd3f6e0c048b7da77ec6dabcf56ff22cc33f1f
Vladislav <mel@mel.rr>
2025-12-11 13:05:39 +0300

    Шаблон админки

---
+++ resources/views/livewire/admin/home-component.blade.php
@@ -0,0 +1,76 @@
+<div class="row">
+
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                            Products
+                        </div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">
+                            {{ $products_cnt }}
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fa-solid fa-boxes-stacked fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-success shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                            Users</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $users_cnt }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fa-solid fa-users fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Earnings (Monthly) Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-info shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Orders
+                        </div>
+                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $orders_cnt }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fa-solid fa-cart-arrow-down fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Pending Requests Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Total Orders</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ Number::currency($orders_total, in: 'USD') }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fa-solid fa-sack-dollar fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Livewire\Cart\Cart;
 use App\Livewire\Cart\CheckoutComponent;
-use App\Livewire\HomeComponent;
 use App\Livewire\Product\CategoryComponent;
 use App\Livewire\Product\ProductComponent;
 use App\Livewire\Search\SearchComponent;
@@ -14,7 +13,7 @@ use App\Livewire\User\OrderShowComponent;
 use App\Livewire\User\RegisterComponent;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeComponent::class)->name('home');
+Route::get('/', \App\Livewire\HomeComponent::class)->name('home');
 Route::get('/product/{slug}', ProductComponent::class)->name('product');
 Route::get('/category/{slug}', CategoryComponent::class)->name('category');
 Route::get('/cart', Cart::class)->name('cart');
@@ -35,4 +34,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/change-account', ChangeAccountComponent::class)->name('change-account');
     Route::get('/orders', OrdersComponent::class)->name('orders');
     Route::get('/order-show/{id}', OrderShowComponent::class)->name('order-show');
+});
+
+Route::prefix('admin')->middleware('admin')->group(function () {
+    Route::get('/', \App\Livewire\Admin\HomeComponent::class)->name('admin');
 });
