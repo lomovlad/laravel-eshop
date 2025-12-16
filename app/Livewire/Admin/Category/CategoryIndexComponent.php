0c15680dc3209063e8629b033c818a7b07cf9efb
Vladislav <mel@mel.rr>
2025-12-16 14:11:30 +0300

    Удаление категорий

--- app/Livewire/Admin/Category/CategoryIndexComponent.php
+++ app/Livewire/Admin/Category/CategoryIndexComponent.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Admin\Category;
 
+use App\Models\Category;
+use App\Models\Product;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -10,6 +12,31 @@ use Livewire\Component;
 #[Title('Categories')]
 class CategoryIndexComponent extends Component
 {
+    public function deleteCategory(Category $category): void
+    {
+        $categories_cnt = $category::query()
+            ->where('parent_id', '=', $category->id)->count();
+
+        if ($categories_cnt) {
+            $this->js("toastr.error('Error! Category has child categories')");
+
+            return;
+        }
+
+        $products_cnt = Product::query()
+            ->where('category_id', '=', $category->id)->count();
+
+        if ($products_cnt) {
+            $this->js("toastr.error('Error! Category has products')");
+
+            return;
+        }
+
+        $category->delete();
+        cache()->forget('categories_html');
+        session()->flash('success', "Category removed");
+        $this->redirectRoute('admin.categories.index', navigate: true);
+    }
     public function render()
     {
         return view('livewire.admin.category.category-index-component');
