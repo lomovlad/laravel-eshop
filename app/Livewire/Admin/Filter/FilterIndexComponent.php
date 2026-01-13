a44c724e200859f002ba83f38a29fb7e1e4f4d82
Vladislav <mel@mel.rr>
2026-01-13 12:15:49 +0300

    Список фильтров

--- app/Livewire/Admin/Filter/FilterIndexComponent.php
+++ app/Livewire/Admin/Filter/FilterIndexComponent.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire\Admin\Filter;
 
+use App\Models\Filter;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Layout('components.layouts.admin')]
+#[Title('Filter List')]
 class FilterIndexComponent extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.admin.filter.filter-index-component');
+        $filters = Filter::query()->with('group')->paginate();
+
+        return view('livewire.admin.filter.filter-index-component', compact('filters'));
     }
 }
