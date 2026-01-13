f5d50a05a39173285c115628b63d80d4da8290ff
Vladislav <mel@mel.rr>
2026-01-13 12:54:22 +0300

    Добавление фильтра

--- app/Livewire/Admin/Filter/FilterCreateComponent.php
+++ app/Livewire/Admin/Filter/FilterCreateComponent.php
@@ -2,16 +2,33 @@
 
 namespace App\Livewire\Admin\Filter;
 
+use App\Models\Filter;
+use App\Models\FilterGroup;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Layout('components.layouts.admin')]
-#[Title('Create Filter Group')]
+#[Title('Create Filter')]
 class FilterCreateComponent extends Component
 {
+    public string $title;
+    public $filter_group_id;
+
+    public function save()
+    {
+        $validated = $this->validate([
+            'title' => 'required|max:255',
+            'filter_group_id' => 'required|exists:filter_groups,id',
+        ]);
+        Filter::query()->create($validated);
+        session()->flash('success', "Filter created successfully");
+        $this->redirectRoute('admin.filters.index', navigate: true);
+    }
     public function render()
     {
-        return view('livewire.admin.filter.filter-create-component');
+        $filter_groups = FilterGroup::all();
+
+        return view('livewire.admin.filter.filter-create-component', compact('filter_groups'));
     }
 }
