729d7fafa4d3150a45522fb2bc0134e1636e9bb3
Vladislav <mel@mel.rr>
2026-01-13 13:26:18 +0300

    Редактирование фильтра

--- app/Livewire/Admin/Filter/FilterEditComponent.php
+++ app/Livewire/Admin/Filter/FilterEditComponent.php
@@ -2,12 +2,41 @@
 
 namespace App\Livewire\Admin\Filter;
 
+use App\Models\Filter;
+use App\Models\FilterGroup;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Layout('components.layouts.admin')]
+#[Title('Edit Filter')]
 class FilterEditComponent extends Component
 {
+    public Filter $filter;
+    public string $title;
+    public $filter_group_id;
+
+    public function mount(Filter $filter)
+    {
+        $this->filter = $filter;
+        $this->title = $filter->title;
+        $this->filter_group_id = $filter->filter_group_id;
+    }
+
+    public function save()
+    {
+        $validated = $this->validate([
+            'title' => 'required|max:255',
+            'filter_group_id' => 'required|exists:filter_groups,id',
+        ]);
+        $this->filter->update($validated);
+        session()->flash('success', "Filter updated successfully");
+        $this->redirectRoute('admin.filters.index', navigate: true);
+    }
     public function render()
     {
-        return view('livewire.admin.filter.filter-edit-component');
+        $filter_groups = FilterGroup::all();
+
+        return view('livewire.admin.filter.filter-edit-component', compact('filter_groups'));
     }
 }
