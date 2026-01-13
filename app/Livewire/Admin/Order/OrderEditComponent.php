9a8246dbc1f07ecbd88beec10f1c29782a21bf4f
Vladislav <mel@mel.rr>
2026-01-13 20:14:10 +0300

    Просмотр и редактирование заказа

--- app/Livewire/Admin/Order/OrderEditComponent.php
+++ app/Livewire/Admin/Order/OrderEditComponent.php
@@ -2,10 +2,29 @@
 
 namespace App\Livewire\Admin\Order;
 
+use App\Models\Order;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Layout('components.layouts.admin')]
+#[Title('Order')]
 class OrderEditComponent extends Component
 {
+    public Order $order;
+    public bool $status;
+
+    public function mount(Order $order)
+    {
+        $this->order = $order;
+        $this->status = $order->status;
+    }
+
+    public function updatedStatus()
+    {
+        $this->order->setAttribute('status', $this->status)->save();
+    }
+
     public function render()
     {
         return view('livewire.admin.order.order-edit-component');
