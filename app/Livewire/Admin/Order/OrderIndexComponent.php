faeb9ef16dfe4c8869012a984b42227543e19699
Vladislav <mel@mel.rr>
2026-01-13 20:21:28 +0300

    Удаление заказа

--- app/Livewire/Admin/Order/OrderIndexComponent.php
+++ app/Livewire/Admin/Order/OrderIndexComponent.php
@@ -3,6 +3,8 @@
 namespace App\Livewire\Admin\Order;
 
 use App\Models\Order;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Title;
 use Livewire\Component;
@@ -14,6 +16,21 @@ class OrderIndexComponent extends Component
 {
     use WithPagination;
 
+
+    public function deleteOrder(Order $order)
+    {
+        try {
+            DB::beginTransaction();
+            $order->orderProducts()->delete();
+            $order->delete();
+            DB::commit();
+            $this->js("toastr.success('Order removed')");
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            $this->js("toastr.error('Error deleting order')");
+        }
+    }
     public function render()
     {
         $orders = Order::query()->orderBy('id', 'desc')->paginate(15);
