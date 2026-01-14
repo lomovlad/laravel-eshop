e8c2ca57bda06da1eb9099e40a0cce10b8f50018
Vladislav <mel@mel.rr>
2026-01-14 12:17:15 +0300

    Добавление юзеров

--- app/Livewire/Admin/User/UserCreateComponent.php
+++ app/Livewire/Admin/User/UserCreateComponent.php
@@ -2,10 +2,38 @@
 
 namespace App\Livewire\Admin\User;
 
+use App\Models\User;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Layout('components.layouts.admin')]
+#[Title('Create User')]
 class UserCreateComponent extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+    public bool $is_admin = false;
+
+    public function save()
+    {
+        $validated = $this->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|min:6',
+            'is_admin' => 'boolean',
+        ]);
+        $user = new User();
+        $user->name = $validated['name'];
+        $user->email = $validated['email'];
+        $user->password = $validated['password'];
+        $user->is_admin = $validated['is_admin'];
+        $user->save();
+        session()->flash('success', "User created successfully");
+        $this->redirectRoute('admin.users.index', navigate: true);
+    }
+
     public function render()
     {
         return view('livewire.admin.user.user-create-component');
--- resources/views/livewire/admin/user/user-create-component.blade.php
+++ resources/views/livewire/admin/user/user-create-component.blade.php
@@ -1,3 +1,78 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="row">
+    <div class="col-12 mb-4 position-relative">
+        <div class="update-loading" wire:loading wire:target="save">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <a href="{{ route('admin.users.index') }}" wire:navigate class="btn btn-primary">Users list</a>
+            </div>
+            <div class="card-body">
+                <form wire:submit="save">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input wire:model="name" type="text"
+                               class="form-control @error('name') is-invalid @enderror" id="name"
+                               placeholder="Name"
+                        >
+                        @error('name')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input wire:model="email" type="email"
+                               class="form-control @error('email') is-invalid @enderror" id="email"
+                               placeholder="Email"
+                        >
+                        @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input wire:model="password" type="password"
+                               class="form-control @error('password') is-invalid @enderror" id="password"
+                               placeholder="Password"
+                        >
+                        @error('password')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        Set Admin rules
+                        <label class="switch">
+                            <input type="checkbox" wire:model="is_admin">
+                            <span class="slider round"></span>
+                        </label>
+                        @error('is_admin')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-info">
+                            Save
+                            <div wire:loading wire:target="save" class="spinner-grow spinner-grow-sm" role="status">
+                                <span class="visually-hidden">Loading...</span>
+                            </div>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 </div>
